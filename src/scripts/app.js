9a4f37a3e14c19c28e4b04acdc9d6fc09d51ca24
Jesse <jesseca.melise@live.com>
2018-10-24 15:12:34 -0400

    made changes in index.html main styles and app.js

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,2 +1,25 @@
-const hello = 'World';
-console.log(hello);
+const app = {}
+
+app.clickEvents = () => {
+    $('.doc:not(.first)').hide()
+    $('.doctors').on('click', function(e){
+        e.preventDefault();
+        if( e.target.nodeName === 'A') {
+            $('.show').show();
+            const chosenDoc = e.target.id;
+            console.log(e.target.id);
+            $('.doc').hide()
+            $(chosenDoc).toggle()
+        } 
+        
+    })
+}
+
+app.init = function() {
+    app.clickEvents();
+    
+    
+}
+$(function(){
+    app.init();
+})
